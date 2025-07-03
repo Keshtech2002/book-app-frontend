55233f22d20908393642643a006608031d0df274
Keshtech2002 <keshtech2002@gmail.com>
2025-07-03 03:24:40 +0100

    first commit

---
+++ src/routers/AdminRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminRoute = ({children}) => {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    return <Navigate to="/admin"/>
+  }
+  return children ? children : <Outlet/>
+}
+
+export default AdminRoute
